bc941c9551a1c1aceb3d645a6e77e1c88a61efa1
pmaddula4 <pranavmaddula36@gmail.com>
2025-02-25 01:12:10 -0600

    added more show functionality to update and rename, made add loop

--- main.py
+++ main.py
@@ -36,43 +36,52 @@ def saveSRankings(ranking):
         json.dump(ranking, file, indent = 4)
     
 def addMovieShow():
-    which = input("Type [w], [m], or [t] to add to the watchlist, movie rankings, or show rankings: ")
-    if which == "w":
-        added = input("Enter the movie/show you want to add to the watchlist: ")
-        valid = False
-        while not valid:
-            addedType = input("Is this a movie or show? ")
-            if addedType == "movie" or addedType == "show":
-                valid = True
-            if addedType == 'n':
-                print()
-                return
-        movies = loadWatchlist()
-        newAdd = {"name": added, "type": addedType}
-        if newAdd not in movies:
-            movies.append(newAdd)
-            saveWatchlist(movies)
-            print(added, "has been added to the watchlist.\n")
+    which = input("Type [w] to add to the watchlist. Type [m] to add to the movie rankings. Type [t] to add to the TV show rankings. Type [b] to go back. ")
+    while True:
+        if which == "w":
+            added = input("Enter the movie/show you want to add to the watchlist: ")
+            valid = False
+            while not valid:
+                addedType = input("Is this a movie or show? ")
+                if addedType == "movie" or addedType == "show":
+                    valid = True
+                if addedType == 'n':
+                    print()
+                    return
+            movies = loadWatchlist()
+            newAdd = {"name": added, "type": addedType}
+            if newAdd not in movies:
+                movies.append(newAdd)
+                saveWatchlist(movies)
+                print(added, "has been added to the watchlist.\n")
+            else:
+                print(added, "already exists in the watchlist.\n")
+            which = input("Type [w] to add to the watchlist. Type [m] to add to the movie rankings. Type [t] to add to the TV show rankings. Type [b] to go back. ")
+        elif which == "m":
+            added = input("Enter the movie you want to add to the rankings: ")
+            mRank = loadMRankings()
+            if added not in mRank:
+                mRank.append(added)
+                saveMRankings(mRank)
+                print(added, "has been added to the movie rankings.\n")
+            else:
+                print(added, "already exists in the movie rankings.\n")
+            which = input("Type [w] to add to the watchlist. Type [m] to add to the movie rankings. Type [t] to add to the TV show rankings. Type [b] to go back. ")
+        elif which == "t":
+            added = input("Enter the show you want to add to the rankings: ")
+            sRank = loadSRankings()
+            if added not in sRank:
+                sRank.append(added)
+                saveMRankings(sRank)
+                print(added, "has been added to the show rankings.\n")
+            else:
+                print(added, "already exists in the show rankings.\n")
+                which = input("Type [w] to add to the watchlist. Type [m] to add to the movie rankings. Type [t] to add to the TV show rankings. Type [b] to go back. ")
+        elif which == "b":
+            break
         else:
-            print(added, "already exists in the watchlist.\n")
-    elif which == "m":
-        added = input("Enter the movie you want to add to the rankings: ")
-        mRank = loadMRankings()
-        if added not in mRank:
-            mRank.append(added)
-            saveMRankings(mRank)
-            print(added, "has been added to the movie rankings.\n")
-        else:
-            print(added, "already exists in the movie rankings.\n")
-    elif which == "t":
-        added = input("Enter the show you want to add to the rankings: ")
-        sRank = loadSRankings()
-        if added not in sRank:
-            sRank.append(added)
-            saveMRankings(sRank)
-            print(added, "has been added to the show rankings.\n")
-        else:
-            print(added, "already exists in the show rankings.\n")
+            print("Invalid input.")
+            which = input("Type [w] to add to the watchlist. Type [m] to add to the movie rankings. Type [t] to add to the TV show rankings. Type [b] to go back. ")
 
 def viewMovieShow():
     which = input("Type [w], [m], or [t] to view the watchlist, movie rankings, or show rankings: ")
@@ -105,7 +114,7 @@ def viewMovieShow():
             print("\nYour TV show rankings are empty.\n")
 
 def removeMovieShow(input):
-    which = input("Type [w], [m], or [t] to remove to the watchlist, movie rankings, or show rankings: ")
+    which = input("Type [w], [m], or [t] to remove from the watchlist, movie rankings, or show rankings: ")
     if which == "w":
         list = loadWatchlist()
         if list:
@@ -144,36 +153,96 @@ def removeMovieShow(input):
             print("Your TV show rankings is empty.\n")
     
     
-def renameMovie(movie):
-    movies = loadWatchlist()
-    if movies:
-        for entry in movies:
-            if entry['name'] == movie:
-                oldName = movie
-                newName = input("Enter the new name of the movie: ")
-                i = movies.index(movie)
-                movies[i] = newName
-                saveWatchlist(movies)
-                print(oldName, " has been updated in the watchlist to \n", newName, ".", sep = "")
+def renameMovieShow(input):
+    which = input("Type [w], [m], or [t] to rename something from the watchlist, movie rankings, or show rankings: ")
+    if which == "w":
+        list = loadWatchlist()
+        if list:
+            for entry in list:
+                if entry['name'] == input:
+                    oldName = input
+                    newName = input("Enter the new name of the movie/show: ")
+                    i = list.index(input)
+                    list[i] = newName
+                    saveWatchlist(list)
+                    print(oldName, " has been updated in the watchlist to \n", newName, ".", sep = "")
+                    return
+            print(input, "does not exist in the watchlist.\n")
+        else:
+            print("Your watchlist is empty.\n")
+    elif which == "m":
+        movies = loadMRankings()
+        if movies:
+            for entry in movies:
+                if entry['name'] == input:
+                    oldName = input
+                    newName = input("Enter the new name of the movie/show: ")
+                    i = movies.index(input)
+                    movies[i] = newName
+                    saveMRankings(movies)
+                    print(oldName, " has been updated in the movie rankings to \n", newName, ".", sep = "")
+                    return
+            print(input, "does not exist in the movie rankings.\n")
+        else:
+            print("Your movie rankings are empty.\n")
+    elif which == "w":
+        shows = loadSRankings()
+        if shows:
+            for entry in shows:
+                if entry['name'] == input:
+                    oldName = input
+                    newName = input("Enter the new name of the show: ")
+                    i = shows.index(input)
+                    shows[i] = newName
+                    saveSRankings(shows)
+                    print(oldName, " has been updated in the TV show rankings to \n", newName, ".", sep = "")
+                    return
+            print(input, "does not exist in the TV show rankings.\n")
+        else:
+            print("Your TV show rankings is empty.\n")
+
+
+def updateOrder(input):
+    which = input("Type [w], [m], or [t] to update the order of the watchlist, movie rankings, or show rankings: ")
+    if which == "w":
+        list = loadWatchlist()
+        for entry in list:
+            if input in list:
+                list.remove(input)
+                newPos = int(input("Enter which position you want to put the movie/show at: "))
+                list.insert(newPos - 1, input)
+                saveWatchlist(list)
+                print(input, "'s position has been updated.\n", sep = "")
                 return
-        print(movie, "does not exist in the watchlist.\n")
-    else:
-        print("Your watchlist is empty.\n")
-
-
-def updateOrder(movie):
-    movies = loadWatchlist()
-    for entry in movies:
-        if movie in movies:
-            movies.remove(movie)
-            newPos = int(input("Enter which position you want to put the movie at: "))
-            movies.insert(newPos - 1, movie)
-            saveWatchlist(movies)
-            print(movie, "'s position has been updated.\n", sep = "")
-            return
-    movies.append(movie)
-    saveWatchlist(movies)
-    print(movie, "has been added to the watchlist.\n")
+        list.append(input)
+        saveWatchlist(list)
+        print(input, "was not in the watchlist, so it has been added to the watchlist.\n")
+    elif which == "m":
+        movies = loadMRankings()
+        for entry in movies:
+            if input in movies:
+                movies.remove(input)
+                newPos = int(input("Enter which position you want to put the movie at: "))
+                movies.insert(newPos - 1, input)
+                saveMRankings(movies)
+                print(input, "'s position has been updated.\n", sep = "")
+                return
+        movies.append(input)
+        saveMRankings(movies)
+        print(input, "was not in the movie rankings, so it has been added to the movie rankings.\n")
+    elif which == "t":
+        shows = loadSRankings()
+        for entry in shows:
+            if input in shows:
+                shows.remove(input)
+                newPos = int(input("Enter which position you want to put the show at: "))
+                shows.insert(newPos - 1, input)
+                saveSRankings(shows)
+                print(input, "'s position has been updated.\n", sep = "")
+                return
+        shows.append(input)
+        saveSRankings(shows)
+        print(input, "was not in the TV show rankings, so it has been added to the TV show rankings.\n")
 
 def clear():
     movies = loadWatchlist()
@@ -196,13 +265,13 @@ def main():
         elif choice == "a":
             addMovieShow()
         elif choice == "d":
-            removed = input("Enter the movie you want to remove from the watchlist: ")
+            removed = input("Enter the movie/show you want to remove from the watchlist: ")
             removeMovieShow(removed)
         elif choice == "r":
-            oldName = input("Enter the movie you want to rename: ")
-            renameMovie(oldName)
+            oldName = input("Enter the movie/show you want to rename: ")
+            renameMovieShow(oldName)
         elif choice == "u":
-            posUpdate = input("Enter the movie who's position you want to update: ")
+            posUpdate = input("Enter the movie/show who's position you want to update: ")
             updateOrder(posUpdate)
         elif choice == "e":
             print()
